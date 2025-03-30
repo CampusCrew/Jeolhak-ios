3ca937afb4eedc9f31a199f4c02a264dc79fa510
YunDaeHyeon <kayuaao96@gmail.com>
2025-03-31 00:37:39 +0900

    feat: 알람 내용, 알람 아이콘 컴포넌트화

--- jeolhak/Controllers/NotificationViewController.swift
+++ jeolhak/Controllers/NotificationViewController.swift
@@ -11,11 +11,29 @@ class NotificationViewController: UIViewController {
     
     private var dateView: UIView!
     
+    private var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dateView = NotificationValueView(image: "star", content: "새로운 가게가 등록되었습니다.")
-        
+        dateView = NotificationDateView(date: "3월 30일")
         view.addSubview(dateView)
+        dateView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // 상단 제약 설정 (safeAreaLayout 기준)
+            dateView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            dateView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        contentView = NotificationContentView(image: "speaker.fill", content: "새로운 가게가 등록되었어요")
+        view.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: dateView.bottomAnchor, constant: 15),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
 }
--- jeolhak/Views/NotificationContentView.swift
+++ jeolhak/Views/NotificationContentView.swift
@@ -31,7 +31,7 @@ class NotificationContentView: UIView {
         
         // 그림자
         layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOpacity = 0.5
+        layer.shadowOpacity = 0.3
         layer.shadowOffset = CGSize(width: 0, height: 3)
         layer.shadowRadius = 6
         layer.masksToBounds = false
@@ -53,8 +53,8 @@ class NotificationContentView: UIView {
         iconBackground.addSubview(icon)
         
         NSLayoutConstraint.activate([
-            icon.centerXAnchor.constraint(equalTo: centerXAnchor),
-            icon.centerYAnchor.constraint(equalTo: centerYAnchor),
+            icon.centerXAnchor.constraint(equalTo: iconBackground.centerXAnchor),
+            icon.centerYAnchor.constraint(equalTo: iconBackground.centerYAnchor),
             icon.widthAnchor.constraint(equalToConstant: 20),
             icon.heightAnchor.constraint(equalToConstant: 20)
         ])
@@ -62,8 +62,29 @@ class NotificationContentView: UIView {
         // 텍스트
         let label = UILabel()
         label.text = content
-        label.font = UIFont(name: "Jua-Regular", size: 16)
-        label.textColor = .white
+        label.font = UIFont(name: "Jua-Regular", size: 20)
+        label.textColor = .systemPink
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        // HStack 생성
+        let stackView = UIStackView(arrangedSubviews: [iconBackground, label])
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            iconBackground.widthAnchor.constraint(equalToConstant: 40),
+            iconBackground.heightAnchor.constraint(equalToConstant: 40),
+            
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12)
+        ])
     }
      
     
