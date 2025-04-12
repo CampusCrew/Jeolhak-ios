1591b931ecfe4335341f8f0b2251214972898a96
YunDaeHyeon <kayuaao96@gmail.com>
2025-04-12 15:52:15 +0900

    feat: Home View 가게 컴포넌트 추가 / 테스트 데이터 삽입

---
+++ jeolhak/Views/HomeContentItemView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeContentItemView.swift
+//  jeolhak
+//
+//  Created by 윤대현 on 4/10/25.
+//
+
+import UIKit
+
+class HomeContentItemView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
